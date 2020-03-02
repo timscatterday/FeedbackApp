ca68d3e8bcee6a8d2eea4b7f5d23be4521e5ec7c
timscatterday <timothyscatterday@outlook.com>
2020-03-02 13:06:24 -0800

    added google keys

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID: '686877979697 - kh457hpf3q8c9qnk7ane3j3gsa7of7n4.apps.googleusercontent.com',
+    googleClientSecret: '-0RuCOgjeN3os4lspDlHQ9tT'
+};
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
 const app = express();
 
-//Routes for our port to listen for
-app.get('/', (req, res) => {
-    res.send({bye: 'buddy'})
-});
+//Creates a new instance of the Google passport strategy
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret
+  })
+);
 
 //Ask heroku which port to listen to or set it to localhost:5000
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
+
