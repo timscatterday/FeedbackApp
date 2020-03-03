b68ab9e467cea6edf46d23d23d737d15a3739d1f
timscatterday <timothyscatterday@outlook.com>
2020-03-03 14:39:41 -0800

    completed google OAuth pattern

--- services/passport.js
+++ services/passport.js
@@ -2,7 +2,19 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 const mongoose = require('mongoose');
+
 const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+    });
+});
 
 passport.use(
     new GoogleStrategy({
@@ -10,7 +22,18 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToke, profile, done) => {
-        new User({ googleId: profile.id }).save();
+        User.findOne({googleId: profile.id})
+         .then((existingUser) => {
+            if(existingUser) {
+                // we already have a record with the given profile Id
+                done(null, existingUser);
+            } else {
+                // we dont have a user record with id, create a new record
+                new User({ googleId: profile.id })
+                    .save()
+                    .then(user => done(null, user));
+            }
+        });
     }
    )
 );
