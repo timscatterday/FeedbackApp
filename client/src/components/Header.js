90e6a6d3b9b3479f943c34a83e7ed0c4fd945b64
timscatterday <timothyscatterday@outlook.com>
2020-03-05 16:28:11 -0800

    added Stripe package and Payments Componenet

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
     renderContent(){
@@ -12,7 +13,10 @@ class Header extends Component {
                     <li><a href='/auth/google'>Login with Google</a></li>
                 )
             default:
-                return <li><a href='/api/logout'>Logout</a></li>
+                return [
+                <li><Payments /></li>,   
+                <li><a href='/api/logout'>Logout</a></li>
+                ]
         }
     };
 
@@ -27,9 +31,7 @@ class Header extends Component {
                         Feedback App
                     </Link>
                     <ul className='right'>
-                        <li>
-                            {this.renderContent()}
-                        </li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
